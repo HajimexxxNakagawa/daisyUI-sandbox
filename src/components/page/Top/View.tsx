1fed8bb654d22b7e77182a3f51ae0af4e5f553bd
Hajime Nakagawa <navyblue0914@icloud.com>
2022-03-06 19:44:50 +0900

    test typography

--- src/components/page/Top/View.tsx
+++ src/components/page/Top/View.tsx
@@ -1,17 +1,21 @@
 import { VFC } from "react"
 
-import { UserTable } from "@/components/model/User/UserTable"
-
-import { Spacer } from "@/components/ui/Spacer"
-
-import { mockUserList } from "@/mocks/User"
-
 export const TopPageView: VFC = () => {
   return (
     <main className="flex flex-col justify-center items-center p-4 h-screen">
-      <h1>Welcome to Nextwind Prototyping Template!</h1>
-      <Spacer size={32} />
-      <UserTable users={mockUserList} />
+      <article className="prose prose-xl">
+        <h1>Garlic bread with cheese: What the science tells us</h1>
+        <p>
+          For years parents have espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </p>
+        <p>
+          But a recent study shows that the celebrated appetizer may be linked
+          to a series of rabies cases springing up around the country.
+        </p>
+      </article>
     </main>
   )
 }
